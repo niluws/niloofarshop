55cdf6629b41fa1abfeb7a95f0742678f6349fb8
niluws <86254611+niluws@users.noreply.github.com>
2022-06-09 20:02:10 +0430

    Add files via upload

--- post_detail/views.py
+++ post_detail/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.views.generic import DetailView
 
+from utils.http_service import get_client_ip
 from .models import Information, ProductVisit
 
 
@@ -9,4 +10,25 @@ class detail(DetailView):
     model = Information
     context_object_name = 'slug'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_product = self.object
+        user_ip = get_client_ip(self.request)
+        user_id = None
 
+        if self.request.user.is_authenticated:
+            user_id = self.request.user.id
+        has_been_visited = ProductVisit.objects.filter(ip__iexact=user_ip, product_id=loaded_product.id).exists()
+
+        if not has_been_visited:
+            new_visit = ProductVisit(ip=user_ip, user_id=user_id, product_id=loaded_product.id)
+            new_visit.save()
+        context['visit'] = ProductVisit.objects.all()
+        context['information']=Information.objects.all()
+        # if ProductVisit.product.cat_id == Information:
+        for a in ProductVisit.objects.all():
+            # for b in a.product.name:
+            if a.product.id == a.id:
+
+                    print(a.product.name,a.ip.count)
+        return context
